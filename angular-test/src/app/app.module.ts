c7379633a831ecf58e8ca6a7568de7285b831370
Johan Leandro Rivera Garzon <johan.rivera@iq-online.com>
2023-01-21 13:46:44 -0500

    Adición de estilos. Pendiente -- Creación Servicios, Modificación componentes - Routing

--- angular-test/src/app/app.module.ts
+++ angular-test/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { AuthenticationComponent } from './modules/authentication/authentication.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AuthenticationComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
